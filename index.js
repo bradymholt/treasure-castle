918f4901716ea9deb2646e0fd59ba9cf1fec911e
Brady Holt <brady.holt@gmail.com>
2020-02-16 23:34:12 +0000

    Use readline-sync for simplicity

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-var inquirer = require("inquirer");
-const readline = require("readline");
+const inquirer = require("inquirer");
+const readline = require("readline-sync");
 
 (async function() {
-  console.log("Welcome to Treasure Castle!");
-  const response = await askQuestion("What is your first name?");
-  console.log(`Hello ${response.answer}!`);
-  await waitForKeyPress(`\
+  
+  print("Welcome to Treasure Castle!");  
+  const response = askQuestion("What is your first name?");
+  print(`Hello ${response}!`);
+  print(`\
 Get ready for adventurous game that will blow\nyour mind and change your life!
 
 The object of this game is to obtain keys to\nthe castle so that you can find
 the treasure and win the game.
 
 To get keys, you must correctly answer a series of VERY HARD questions.
-
-[Press any key to play]
 `);
+
+  await waitForKeyPress("[Press any key to play]");
 
   clearScreen();
 
-  const questions = [
+  for (let question of questions) {
+    const response = await inquirer.prompt([
+      {
+        type: "list",
+        name: "answer",
+        message: question.text,
+        choices: question.choices
+      }
+    ]);
+
+    const enteredAnswer = response.answer;
+    const correctAnswer = question.answer;
+    if (enteredAnswer == correctAnswer) {
+      console.log("CORRECT!");
+    } else {
+      console.log("YOU LOSE!");
+    }
+    await waitForKeyPress("[Press any key to continue]");
+
+    clearScreen();
+  }
+})();
+
+const questions = [
     {
       text: "What color is the sky?",
       choices: ["Blue", "Ultraviolet", "White"],
@@ -31,56 +55,18 @@ To get keys, you must correctly answer a series of VERY HARD questions.
     }
   ];
 
-  for (let question of questions) {
-    const response = await inquirer.prompt([
-      {
-        type: "list",
-        name: "answer",
-        message: question.text,
-        choices: question.choices
-      }
-    ]);
-
-    const youAnswered = response.answer;
-    const correctAnswer = question.answer;
-    console.log(`You answered: '${youAnswered}'`);
-    if (youAnswered == correctAnswer) {
-      console.log("CORRECT!");
-    } else {
-      console.log("YOU LOSE!");
-    }
-    await waitForKeyPress("Press any key to continue");
-
-    clearScreen();
-  }
-})();
+function print(message){
+  console.log(message);
+}
 
 function clearScreen() {
-  process.stdout.write("\u001b[2J\u001b[0;0H");
+  console.clear();
 }
 
-function waitForKeyPress() {}
-
-function waitForKeyPress(query) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  return new Promise(resolve =>
-    rl.question(query, ans => {
-      rl.close();
-      resolve(ans);
-    })
-  );
+function waitForKeyPress(message) {
+  return askQuestion("[Press any key to play]")
 }
 
-async function askQuestion(question) {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "answer",
-      message: question
-    }
-  ]);  
+function askQuestion(question) {  
+  return readline.question(question + "\n");
 }
